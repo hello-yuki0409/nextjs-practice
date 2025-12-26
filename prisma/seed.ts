5440be1a52dfb261163377e3b95c3e4123d2c117
hello-yuki0409 <bobu2784@gmail.com>
2025-12-26 15:20:14 +0900

    feat: DB作成

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: "Alice",
+    email: "alice@prisma.io",
+    password: "password",
+  },
+  {
+    name: "Bob",
+    email: "bob@prisma.io",
+    password: "password",
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
