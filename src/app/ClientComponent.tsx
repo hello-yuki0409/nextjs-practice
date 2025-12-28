84af0ecee8d5b427ca584802317ac4721400188d
hello-yuki0409 <bobu2784@gmail.com>
2025-12-28 14:49:07 +0900

    feat: サーバーコンポーネント

--- src/app/ClientComponent.tsx
+++ src/app/ClientComponent.tsx
@@ -1,14 +1,35 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 const ClientComponent = () => {
-    const [count, setCount] = useState(0);
-  return (
-    <>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </>
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const onChangeData = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setData ({...data, [name]: value})
+    }
+
+    const onSubmit = () => {
+        fetch("/users", {
+            method: "POST",
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json()
+        .then((res) => console.log({res})
+        ))
+    }
+
+return (
+    <div>
+      <input className="border" type="text" name="name" onChange={onChangeData} />
+      <input className="border" type="text" name="email" onChange={onChangeData} />
+      <input className="border" type="text" name="password" onChange={onChangeData} />
+      <button onClick={onSubmit}>Submit送信</button>
+    </div>
   )
 }
 
